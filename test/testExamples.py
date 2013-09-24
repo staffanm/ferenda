fa499162db5a8343a039740d68712b6b5f367c6b
Staffan Malmgren <staffan.malmgren@gmail.com>
2013-09-24 22:09:46 +0200

    ongoing work

---
+++ test/testExamples.py
@@ -0,0 +1,69 @@
+import sys
+import os
+import subprocess
+import tempfile
+import shutil
+
+import six
+
+from ferenda import util
+from ferenda.compat import unittest, patch
+from ferenda.testutil import FerendaTestCase
+
+# This testcase tests those examples in the documentation that are
+# more unit-like and can run without downloading stuff from the
+# net. More integration-like tests are in integrationTestExamples (and
+# thus aren't run in the normal test suite).
+
+# imports needed by the scripts. I do not fully understand exactly how
+# imports are scoped when using exec, but this is the only way apart
+# from importing inside of the functions that use the code to work.
+from ferenda import elements, DocumentRepository, DocumentStore
+from bs4 import BeautifulSoup
+import requests
+from six.moves.urllib_parse import urljoin
+
+
+class TestExamples(unittest.TestCase, FerendaTestCase):
+    def _test_pyfile(self, pyfile, want=True, comparator=None):
+        pycode = compile(util.readfile(pyfile), pyfile, 'exec')
+        result = six.exec_(pycode, globals(), locals())
+        # the exec:ed code is expected to set return_value
+        got = locals()['return_value']
+        if not comparator:
+            comparator = self.assertEqual
+        comparator(want, got)
+
+    def test_elementclasses(self):
+        # setup w3standards.py -- modify sys.path?
+        self._test_pyfile("doc/examples/elementclasses.py",
+                          util.readfile("doc/examples/elementclasses-part.xhtml", "rb"),
+                          self.assertEqualXML)
+
+    def test_fsmparser_example(self):
+        self._test_pyfile("doc/examples/fsmparser-example.py",
+                          util.readfile("doc/examples/fsmparser-result.xml"),
+                          self.assertEqualXML)
+
+    def test_keyconcepts_attachments(self):
+        with patch('requests.get'):
+            self._test_pyfile("doc/examples/keyconcepts-attachments.py")
+
+    def test_keyconcepts_file(self):
+        self._test_pyfile("doc/examples/keyconcepts-file.py")
+
+    def test_metadata(self):
+        self._test_pyfile("doc/examples/metadata.py",
+                          util.readfile("doc/examples/metadata-result.xml"),
+                          self.assertEqualXML)
+
+    def test_citationparsing_urls(self):
+        self._test_pyfile("doc/examples/citationparsing-urls.py")
+        
+    def test_citationparsing_parsers(self):
+        self._test_pyfile("doc/examples/citationparsing-parsers.py",
+                          util.readfile("doc/examples/citationparsing-after.xhtml"),
+                          self.assertEqualXML)
+        
+    def test_citationparsing_custom(self):
+        self._test_pyfile("doc/examples/citationparsing-custom.py")
