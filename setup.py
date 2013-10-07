464dd6bf6464ea63770d5db4180108e9e69e2a04
Staffan Malmgren <staffan.malmgren@gmail.com>
2013-10-07 23:03:48 +0200

    docutils is now required

--- setup.py
+++ setup.py
@@ -18,7 +18,8 @@ install_requires = ['beautifulsoup4 >= 4.3.0',
                     'requests >= 1.2.0',
                     'Whoosh >= 2.4.1',
                     'six >= 1.4.0',
-                    'pyparsing >= 2.0.1']
+                    'pyparsing >= 2.0.1',
+                    'docutils >= 0.11']
 
 if sys.version_info < (3,0,0):
     # not py3 compatible, but not essential either
