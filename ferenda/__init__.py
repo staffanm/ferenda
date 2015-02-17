caaa20e71b8f0ff841ae4ba6ac3cdcdb1dc9dea6
Staffan Malmgren <staffan.malmgren@gmail.com>
2015-02-17 20:41:02 +0100

    escape backslashes in yml

--- ferenda/__init__.py
+++ ferenda/__init__.py
@@ -24,16 +24,4 @@ from .pdfdocumentrepository import PDFDocumentRepository
 from .compositerepository import CompositeRepository, CompositeStore
 from .resources import Resources
 from .wsgiapp import WSGIApp
-__version__ = "0.3.0.dev1" #gets pulled into setup.py and docs/conf.py
-# dev1: changes constructor signature for DocumentRepository (using a config obj as first positional parameter)
-# dev2: enables multiprocessing for manager task queue handling (./ferenda.py rfc parse --all --processes=4)
-# dev3: incorporation of changes in ferenda.sources.legal.se during lagen.nu-tng experimentation
-# dev4: Element.as_xhtml() now creates dct:isPartOf triples for sectioned documents
-# dev5: bundled a git snapshot (4e339f0) of swc.mw (for MediaWiki markup) as ferenda.thirdparty.mw 
-# dev6: more incorporation of changes in ferenda.sources.legal.se during lagen.nu-tng experimentation
-# dev7: changes in toc handling
-# dev8: LayeredConfig.set (used by manager._instantiate_class)
-# dev9: buildclient, buildqueue, --buildserver and --buildqueue support
-# dev10: LayeredConfig separate package, no longer bundled.
-# dev11: Feed functionality uses facets, New PDFAnalyzer, testutil.download_test supports FERENDA_SET_TESTFILES...
-# 0.3.0.dev1: Minor version bump
+__version__ = "0.3.0"  # gets pulled into setup.py and docs/conf.py -- but appveyor.yml is separate
