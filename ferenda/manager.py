staffanm/ferenda

commit eed5ca49c1ce61bfc578bf31ba4e8cc30ee7b51a

regeringen.py now handles scanned pdfs by OCR:ing them. The OCR functionality now internally converts intermediate files to PNG instead of a random NetPBM file ending

ferenda/pdfreader.py
@@ -204,17 +204,17 @@ class PDFReader(CompoundElement):
             topage = min((i + 1) * 10, number_of_pages)
             if frompage > topage:
                 continue
-            cmd = "pdfimages -p -f %(frompage)s -l %(topage)s %(tmppdffile)s %(workdir)s/%(root)s" % locals(
+            cmd = "pdfimages -png -p -f %(frompage)s -l %(topage)s %(tmppdffile)s %(workdir)s/%(root)s" % locals(
             )
             self.log.debug("- running " + cmd)
             (returncode, stdout, stderr) = util.runcmd(cmd, require_success=True)
-            # step 2.1: Combine the recently extracted images and
-            # into a new tif (so that we add 10
-            # pages at a time to the tif, as imagemagick can
-            # create a number of pretty large files for each page,
-            # so converting 200 images will fill 10 G of your temp
-            # space -- which we'd like to avoid)
-            cmd = "convert %(workdir)s/%(root)s-*.pbm -compress Zip %(workdir)s/%(root)s-tmp%(idx)04d.tif" % locals(
+            # step 2.1: Combine the recently extracted images (which
+            # can be pbm or ppm files) and into a new tif (so that we
+            # add 10 pages at a time to the tif, as imagemagick can
+            # create a number of pretty large files for each page, so
+            # converting 200 images will fill 10 G of your temp space
+            # -- which we'd like to avoid)
+            cmd = "convert %(workdir)s/%(root)s-*.png -compress Zip %(workdir)s/%(root)s-tmp%(idx)04d.tif" % locals(
             )
             self.log.debug("- running " + cmd)
             (returncode, stdout, stderr) = util.runcmd(cmd, require_success=True)

ferenda/pdfanalyze.py
@@ -276,6 +276,8 @@ class PDFAnalyzer(object):
             if charcount > maxcount:
                 header = i - 1
                 break
+        else:
+             header = maxcount
         charcount = 0
         maxcount = self.footer_significance_threshold * sum(vcounters['topmargin'].values())
         for i in range(max(vcounters['pageheight']) - 1, -1, -1):
@@ -283,6 +285,8 @@ class PDFAnalyzer(object):
             if charcount > maxcount:
                 footer = i + 1
                 break
+        else:
+             footer = maxcount   
         return {'topmargin': header,
                 'bottommargin': footer}
 

ferenda/sources/legal/se/regeringen.py
@@ -323,7 +323,12 @@ class Regeringen(SwedishLegalSource):
         a["dcterms:identifier"] = a["dcterms:identifier"].replace("ID-nummer: ", "")
         # save for later
         self._identifier = a["dcterms:identifier"]
-        a["rpubl:departement"] = self.lookup_resource(a["rpubl:departement"])
+        # it's rare, but in some cases a document can be published by
+        # two different departments (eg dir. 2011:80). Convert string
+        # to a list in these cases (SwedishLegalSource.polish_metadata
+        # will handle that)
+        if ", " in a["rpubl:departement"]:
+            a["rpubl:departement"] = a["rpubl:departement"].split(", ")
         # remove empty utgarFran list
         if a["rpubl:utgarFran"]:
             a["rpubl:utgarFran"] = [URIRef(x) for x in a["rpubl:utgarFran"]]
@@ -540,6 +545,17 @@ class Regeringen(SwedishLegalSource):
                                    workdir=intermediatedir,
                                    images=self.config.pdfimages,
                                    keep_xml=keep_xml)
+        if pdf.is_empty:
+            self.log.warning("PDF file %s had no textcontent, trying OCR" % pdffile)
+            # No use using the FontmappingPDFReader, since OCR:ed
+            # files lack the same fonts as that reader can handle.
+            pdf = PDFReader(filename=pdffile,
+                            workdir=intermediatedir,
+                            images=self.config.pdfimages,
+                            keep_xml=keep_xml,
+                            ocr_lang="swe")
+
+            
         return pdf
 
     # returns a list of (PDFReader, metrics) tuples, one for each PDF

commit 7b49547d1c6feea0780959acb1c1109212be10de

fixed additional bug caused by eed5ca4 where png files werent removed

ferenda/manager.py
@@ -1577,7 +1577,7 @@ def _preflight_check(log, verbose=False):
         ('whoosh', '2.4.1', True),
         ('pyparsing', '1.5.7', True))
 
-    binaries = (('pdftotext', '-v'),
+    binaries = (('pdftotext', '-v'),  # FIXME: we also now require pdfimages, at least version 0.25 (which supports the -png flag)
                 ('pdftohtml', '-v'),
                 ('antiword', '-h'),
                 ('convert', '-version'),

ferenda/pdfreader.py
@@ -209,17 +209,17 @@ class PDFReader(CompoundElement):
             self.log.debug("- running " + cmd)
             (returncode, stdout, stderr) = util.runcmd(cmd, require_success=True)
             # step 2.1: Combine the recently extracted images (which
-            # can be pbm or ppm files) and into a new tif (so that we
-            # add 10 pages at a time to the tif, as imagemagick can
-            # create a number of pretty large files for each page, so
-            # converting 200 images will fill 10 G of your temp space
-            # -- which we'd like to avoid)
+            # are always png) into a new tif (so that we add 10 pages
+            # at a time to the tif, as imagemagick can create a number
+            # of pretty large files for each page, so converting 200
+            # images will fill 10 G of your temp space -- which we'd
+            # like to avoid)
             cmd = "convert %(workdir)s/%(root)s-*.png -compress Zip %(workdir)s/%(root)s-tmp%(idx)04d.tif" % locals(
             )
             self.log.debug("- running " + cmd)
             (returncode, stdout, stderr) = util.runcmd(cmd, require_success=True)
-            # step 2.2: Remove pbm files now that they're in the .tif
-            for f in glob("%(workdir)s/%(root)s-*.pbm" % locals()):
+            # step 2.2: Remove png files now that they're in the .tif
+            for f in glob("%(workdir)s/%(root)s-*.png" % locals()):
                 os.unlink(f)
 
         # Step 3: Combine all the 10-page tifs into a giant tif using tiffcp

ferenda/sources/legal/se/sfs.py
@@ -1338,7 +1338,7 @@ class SFS(Trips):
     def get_parser(self, basefile, sanitized):
         # this should work something like offtryck_parser
         from .sfs_parser import make_parser
-        return make_parser(sanitized, self.log, self.trace)
+        return make_parser(sanitized, basefile, self.log, self.trace)
 
     def visitor_functions(self, basefile):
         return ((self.set_skipfragments, None),

ferenda/sources/legal/se/sfs_parser.py
@@ -46,9 +46,24 @@ re_roman_numeral_matcher = re.compile(
 state = {'current_section': '0',
          'current_headline_level': 0}  # 0 = unknown, 1 = normal, 2 = sub
 
-def make_parser(reader, log, trace):
+swedish_ordinal_list = ('f\xf6rsta', 'andra', 'tredje', 'fj\xe4rde',
+                        'femte', 'sj\xe4tte', 'sjunde', '\xe5ttonde',
+                        'nionde', 'tionde', 'elfte', 'tolfte')
+
+swedish_ordinal_dict = dict(list(zip(
+    swedish_ordinal_list, list(range(1, len(swedish_ordinal_list) + 1)))))
+
+def _swedish_ordinal(s):
+    """'första' => '1'"""
+    sl = s.lower()
+    if sl in swedish_ordinal_dict:
+        return swedish_ordinal_dict[sl]
+    return None
+
+def make_parser(reader, basefile, log, trace):
     state['current_section'] = '0'
     state['current_headline_level'] = 0
+    state['basefile'] = basefile
 
     def parse(the_reader):
         global reader
@@ -237,7 +252,7 @@ def make_parser(reader, log, trace):
             elif state_handler == makeOvergangsbestammelse:
                 log.debug("      Paragraf %s färdig" % paragrafnummer)
                 log.warning(
-                    "%s: Avskiljande rubrik saknas mellan författningstext och övergångsbestämmelser" % id)
+                    "%s: Avskiljande rubrik saknas mellan författningstext och övergångsbestämmelser" % state['basefile'])
                 return p
             else:
                 assert state_handler == makeStycke, "guess_state returned %s, not makeStycke" % state_handler.__name__
@@ -374,10 +389,10 @@ def make_parser(reader, log, trace):
             if res is not None:
                 if state_handler != makeOvergangsbestammelse:
                     # assume these are the initial Övergångsbestämmelser
-                    if hasattr('id'):
-                        sfsnr = id
+                    if 'basefile' in state:
+                        sfsnr = state['basefile']
                         log.warning(
-                            "%s: Övergångsbestämmelsen saknar SFS-nummer - antar %s" % (id, sfsnr))
+                            "%s: Övergångsbestämmelsen saknar SFS-nummer - antar %s" % (state['basefile'], sfsnr))
                     else:
                         sfsnr = '0000:000'
                         log.warning(
@@ -1123,7 +1138,7 @@ def make_parser(reader, log, trace):
             fuzz = difflib.get_close_matches(l, separators, 1, 0.9)
             if fuzz:
                 log.warning("%s: Antar att '%s' ska vara '%s'?" %
-                                 (id, l, fuzz[0]))
+                                 (state['basefile'], l, fuzz[0]))
             else:
                 return False
         try:

ferenda/sources/legal/se/swedishlegalsource.py
@@ -78,13 +78,6 @@ class SwedishLegalSource(DocumentRepository):
     required_predicates = [RDF.type, DCTERMS.title, DCTERMS.issued,
                            DCTERMS.identifier, PROV.wasGeneratedBy]
 
-    swedish_ordinal_list = ('f\xf6rsta', 'andra', 'tredje', 'fj\xe4rde',
-                            'femte', 'sj\xe4tte', 'sjunde', '\xe5ttonde',
-                            'nionde', 'tionde', 'elfte', 'tolfte')
-
-    swedish_ordinal_dict = dict(list(zip(
-        swedish_ordinal_list, list(range(1, len(swedish_ordinal_list) + 1)))))
-
     swedish_months = {"januari": 1,
                       "jan": 1,
                       "februari": 2,
@@ -803,12 +796,6 @@ class SwedishLegalSource(DocumentRepository):
     ################################################################
     # General small utility functions
     # (these could be module functions or staticmethods instead)
-    def _swedish_ordinal(self, s):
-        """'första' => '1'"""
-        sl = s.lower()
-        if sl in self.swedish_ordinal_dict:
-            return self.swedish_ordinal_dict[sl]
-        return None
 
     def parse_iso_date(self, datestr):
         # only handles YYYY-MM-DD now. Look into dateutil or isodate