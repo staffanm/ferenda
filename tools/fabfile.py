3ae919d8c4b932806ac855466e745100cb123085
Staffan Malmgren <staffan.malmgren@gmail.com>
2016-03-08 21:55:27 +0100

    copy_elastic command now works, maybe others also

--- tools/fabfile.py
+++ tools/fabfile.py
@@ -1,4 +1,4 @@
-from fabric.api import run, local, cd
+from fabric.api import run, local, cd, sudo
 from fabric.contrib.project import rsync_project
 from datetime import datetime
 
@@ -9,29 +9,35 @@ def doccount():
 
 # run from i7 with fab -H colo.tomtebo.org -f tools/fabfile.py copy_elastic
 def copy_elastic():
-    snapshot_id = datetime.now().strftime("%d%m%y-%H%M%S")
+    snapshot_id = datetime.now().strftime("%y%m%d-%H%M%S")
     # compute new snapshot id YYYYMMDD-HHMMSS
-    snapshot_url = "http://localhost:9200/_snaphost/lagen_backup/%s?wait_for_completion=true" % snapshot_id
+    snapshot_url = "http://localhost:9200/_snapshot/lagen_backup/%s?wait_for_completion=true" % snapshot_id
 
     # calculate doccount
-    local_doccount = local("curl -s http://localhost:9200/lagen/_count?pretty=true|grep count")
+    local_doccount = local("curl -s http://localhost:9200/lagen/_count?pretty=true|grep count", capture=True)
 
-    # requests.put new snapshot id
-    local("curl -XPUT %s" % snapshot_url)
+    # create a new snapshot with the computed id
+    local('curl -f -XPUT \'%s\' -d \'{ "indices": "lagen"}\'' % snapshot_url)
     
     # rsync /tmp/elasticsearch/snapshots from local to target (must be
     # same locally and on target)
     snapshotdir = "/tmp/elasticsearch/snapshot/lagen/"
+    sudo("chown -R staffan:staffan %s" % snapshotdir)
     rsync_project(local_dir=snapshotdir,
                   remote_dir=snapshotdir,
                   delete=True)
+    sudo("chown -R elasticsearch:elasticsearch %s" % snapshotdir)
 
-    # on target, curl POST to restore snapshot (maybe we need to
-    # restart ES before?)
-    run("curl -XPOST http://localhost:9200/_snapshot/lagen_backup/%s/_restore?wait_for_completion=true" % snapshot_id)
+    # on target, curl POST to restore snapshot (close index beforehand
+    # and open it after)
+    run("curl -XPOST http://localhost:9200/lagen/_close")
+    run("curl -f -XPOST http://localhost:9200/_snapshot/lagen_backup/%s/_restore?wait_for_completion=true" % snapshot_id)
+    run("curl -XPOST http://localhost:9200/lagen/_open")
+
     # on target, calculate doccount and compare
     remote_doccount = run("curl -s http://localhost:9200/lagen/_count?pretty=true|grep count")
     assert local_doccount == remote_doccount
+
 
 # run from i7 with fab -H colo.tomtebo.org -f tools/fabfile.py copy_elastic
 def copy_files():
@@ -42,11 +48,18 @@ def copy_files():
                   delete=True)
 
 
-def pullgit():
+def git_pull():
     with cd("~/wds/ferenda"):
         run("git pull --rebase")
 
 
-def installrdf():
+def import_rdf():
     with cd("~/www/ferenda.lagen.nu"):
         run("~/.virtualenvs/lagen.nu/bin/python ./ferenda-build.py devel rdfimport")
+
+
+def deploy():
+    git_pull()
+    copy_files()
+    copy_elastic()
+    # import_rdf()
