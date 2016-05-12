6e537c34a80d18d01ed6d0768a1ee0996424fe22
Staffan Malmgren <staffan.malmgren@gmail.com>
2016-05-12 20:19:01 +0200

    simple script to analyze a log for the most common errors and which repos have the most errors

---
+++ tools/analyze-error-log.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+import sys
+import re
+from collections import defaultdict
+
+def analyze_log(filename):
+    modules = defaultdict(int)
+    locations = defaultdict(int)
+    locationmsg = {}
+    with open(filename) as fp:
+        for line in fp:
+            timestamp, module, level, message = line.split(" ", 3)
+            if level == "ERROR":
+                realmodule = message.split(" ", 1)[0]
+                modules[realmodule] += 1
+            m = re.search("\([\w/]+.py:\d+\)", message)
+            if m:
+                location = m.group(0)
+                locations[location] += 1
+                if location not in locationmsg:
+                    locationmsg[location] = message.strip()
+    print("Top error modules:")
+    printdict(modules)
+    print("Top error messages:")
+    printdict(locations, locationmsg) 
+
+def printdict(d, labels=None):
+    # prints out a dict with int values, sorted by these
+    for k in sorted(d, key=d.get, reverse=True):
+        if labels:
+            lbl = labels[k]
+        else:
+            lbl = k
+        print("%4d %s" % (d[k], lbl))
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("USAGE: %s logfilename" % sys.argv[0])
+    else:
+        analyze_log(sys.argv[1])
