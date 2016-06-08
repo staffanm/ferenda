b497647982884b83f206f0fe0c64fd81552c7426
Staffan Malmgren <staffan.malmgren@gmail.com>
2016-06-08 15:52:51 +0200

    only validate XHTML+RDFa triples if config.validaterdfa is set

--- ferenda/decorators.py
+++ ferenda/decorators.py
@@ -247,28 +247,29 @@ def render(f):
                 self.log.warning("%s: Metadata is missing a %s triple" %
                                  (doc.basefile, distilled_graph.qname(p)))
 
-        # Validate that all triples specified in doc.meta and any
-        # .meta property on any body object is present in the
-        # XHTML+RDFa file.  NOTE: graph_diff has suddenly become
-        # glacial on medium-large graphs (> ~1000 triples). Maybe we
-        # don't have to validate them?
-        huge_graph = False
-        for g in iterate_graphs(doc.body):
-            doc.meta += g
-            if len(doc.meta) > 1000:
-                huge_graph = True
-                break
-        if huge_graph:
-            self.log.warning("%s: Graph seems huge, skipping validation" % doc.basefile)
-        else:
-            # self.log.debug("%s: diffing graphs" % doc.basefile)
-            (in_both, in_first, in_second) = graph_diff(doc.meta, distilled_graph)
-            self.log.debug("%s: graphs diffed (-%s, +%s)" % (doc.basefile, len(in_first), len(in_second)))
+        if 'validaterdfa' in self.config and self.config.validaterdfa:
+            # Validate that all triples specified in doc.meta and any
+            # .meta property on any body object is present in the
+            # XHTML+RDFa file.  NOTE: graph_diff has suddenly become
+            # glacial on medium-large graphs (> ~1000 triples). Maybe we
+            # don't have to validate them?
+            huge_graph = False
+            for g in iterate_graphs(doc.body):
+                doc.meta += g
+                if len(doc.meta) > 1000:
+                    huge_graph = True
+                    break
+            if huge_graph:
+                self.log.warning("%s: Graph seems huge, skipping validation" % doc.basefile)
+            else:
+                # self.log.debug("%s: diffing graphs" % doc.basefile)
+                (in_both, in_first, in_second) = graph_diff(doc.meta, distilled_graph)
+                self.log.debug("%s: graphs diffed (-%s, +%s)" % (doc.basefile, len(in_first), len(in_second)))
 
-            if in_first:  # original metadata not present in the XHTML filee
-                self.log.warning("%s: %d triple(s) from the original metadata was "
-                                 "not found in the serialized XHTML file:\n%s",
-                                 doc.basefile, len(in_first), in_first.serialize(format="n3").decode("utf-8"))
+                if in_first:  # original metadata not present in the XHTML filee
+                    self.log.warning("%s: %d triple(s) from the original metadata was "
+                                     "not found in the serialized XHTML file:\n%s",
+                                     doc.basefile, len(in_first), in_first.serialize(format="n3").decode("utf-8"))
 
         # Validate that entry.title and entry.id has been filled
         # (might be from doc.meta and doc.uri, might be other things
