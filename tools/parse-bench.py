f9ecbd27fd3d8750bb4d690f8a4d5fd204f573ca
Staffan Malmgren <staffan.malmgren@gmail.com>
2016-06-09 16:36:57 +0200

    rewrote and broke everything in order to also be able to test LegalRef instances (no docrepo) directly.

--- tools/parse-bench.py
+++ tools/parse-bench.py
@@ -21,16 +21,65 @@ sys.path.append(os.path.normpath(os.getcwd() + os.sep + os.pardir))
 from ferenda.manager import _load_class, _load_config, _find_config_file, DEFAULT_CONFIG
 from ferenda.elements import deserialize, Link
 from ferenda import util
+  
+from test.functionalLegalRef import TestLegalRef
+
+class LegalRefTest(object):
+    def __init__(self, alias):
+        # setup 
+        parsetype = alias.split("/")[1]
+        self.parser = LegalRef({'SFS': LegalRef.LAGRUM,
+                                'Short': LegalRef.KORTLAGRUM,
+                                'DV': LegalRef.RATTSFALL,
+                                'Regpubl': LegalRef.FORARBETEN,
+                                'EGLag': LegalRef.EULAGSTIFTNING,
+                                'ECJ': LegalRef.EGRATTSFALL}[parsetype]
+        # attempt to run a classmethod for one class and 
+        # have it modifying another. This can't possibly 
+        # work for a bound method?
+        TestLegalRef.setUpClass(self.__class__)
+       
+    def createtest(basefile, basedir):
+        # FIXME: TestLegalRef._parse should be MOST of ._test_parser, without 
+        # the ending assertEqual (and preferably w/o serialize as well)
+        testfile = basedir + basefile + ".txt"
+        start = time.time()
+        nodes = TestLegalRef._parse(self, testfile, self.parser)
+        elapsed = time.time() - start
+        return elapsed, extractrefs(nodes)
+
+    timetest = createtest
 
 
-def initialize_repo(config, alias):
-    repoconfig = getattr(config, alias)
-    classname = getattr(repoconfig, 'class')
-    repocls = _load_class(classname)
-    repo = repocls()
-    repo.config = repoconfig
-    return repo
-    
+class RepoTest(object):
+
+    def __init__(config, alias):
+        repoconfig = getattr(config, alias)
+        classname = getattr(repoconfig, 'class')
+        repocls = _load_class(classname)
+        self.repo = repocls()
+        self.repo.config = repoconfig
+
+
+    def timetest(basefile, basedir):
+        serialized_path = self.repo.store.serialized_path(basefile) + ".unparsed"
+        serialized_path.replace(self.repo.store.datadir, basedir)
+        with codecs.open(serialized_path, "r", encoding="utf-8") as fp:
+            doc = deserialize(fp.read(), format="json")
+        refparser = self.repo.refparser
+        start = time.time()
+        refparser.parse_recursive(doc)
+        elapsed = time.time() - start
+        return elapsed, extractrefs(doc)
+
+    def createtest(basefile, basedir):
+        print("parsing %s/%s" % (self.repo.alias, basefile))
+        self.repo.config.force = True # make this dependent on whether 
+        self.repo.parse(basefile)
+        # refs = refs_from_graph(Graph().parse(repo.store.distilled_path(basefile)))
+        elapsed, refs = timetest(repo, basefile, basedir)
+        return elapsed, refs
+
 def extractrefs(node):
     if isinstance(node, Link):
         return [node.uri]
@@ -44,35 +93,20 @@ def extractrefs(node):
         except TypeError:  # subnode object is not iterable
             pass
         return res
-            
-
-def timetest(repo, basefile, basedir):
-    serialized_path = repo.store.serialized_path(basefile) + ".unparsed"
-    serialized_path.replace(repo.store.datadir, basedir)
-    with codecs.open(serialized_path, "r", encoding="utf-8") as fp:
-        doc = deserialize(fp.read(), format="json")
-    refparser = repo.refparser
-    start = time.time()
-    refparser.parse_recursive(doc)
-    elapsed = time.time() - start
-    return elapsed, extractrefs(doc)
-
-
-# - A function (createtest) taking a docrepo alias and basefile. Runs
-#   parse on that file, injects something that prior to parseref dumps
-#   the document as JSON, then post parseref dumps triples to n3
-def createtest(repo, basefile, basedir):
-    print("parsing %s/%s" % (repo.alias, basefile))
-    repo.config.force = True # make this dependent on whether 
-    repo.parse(basefile)
-    # refs = refs_from_graph(Graph().parse(repo.store.distilled_path(basefile)))
-    elapsed, refs = timetest(repo, basefile, basedir)
-    return elapsed, refs
 
 def getconfig(basedir):
     defaults = dict(DEFAULT_CONFIG)
     os.environ['FERENDA_SERIALIZEUNPARSED'] = basedir
     return _load_config(_find_config_file())
+
+
+def time_legalref_test(alias, test) # 
+    # leverage the setUpClass and _test_parser methods from test/integrationLegalRef somehow
+    parser = integrationLegalRef.setUpClass()
+    start = time.time()
+    nodes = integrationLegalRef._testParse(test, parser)
+    elapsed = time.time() - start
+    return elapsed, refs_from_graph(nodes)
 
     
 def createtestsuite(testsuitefile):
@@ -82,13 +116,19 @@ def createtestsuite(testsuitefile):
     config = getconfig(basedir)
     baseline = {}
     for alias, basefiles in testsuite.items():
-        repo = initialize_repo(config, alias)
+        if alias.startswith("legalref/"):
+            tester = LegalRefTest(alias)
+            if not basefiles:
+                basefiles = (x[11+len(alias):-4] for x in util.listdir("test/files/" + alias, suffix=".txt"))
+        else:
+            tester = RepoTest(config, alias)
         baseline[alias] = []
         for basefile in basefiles:
-            elapsed, refgraph = createtest(repo, basefile, basedir)
+            elapsed, refgraph = tester.createtest(basefile, basedir)
             baseline[alias].append({'basefile': basefile,
                                     'elapsed': elapsed,
                                     'refgraph': refgraph})
+
     baselinefile = testsuitefile.replace(".json", ".baseline.json")
     if os.path.exists(baseline):
         with open(baselinefile) as fp:
@@ -104,11 +144,14 @@ def evaltestsuite(testsuitefile):
     config = getconfig(basedir)
     results = {}
     for alias, basefiles in testsuite.items():
-        repo = initialize_repo(config, alias)
+        if alias.startswith("legalref/"):
+            tester = LegalRefTest(alias)
+	else:
+            tester = RepoTest(config, alias)
         results[alias] = []
         for basefile in basefiles:
             print("testing %s/%s" % (alias, basefile))
-            elapsed, refgraph = timetest(repo, basefile, basedir)
+            elapsed, refgraph = tester.timetest(basefile, basedir)
             results[alias].append({'basefile': basefile,
                                    'elapsed': elapsed,
                                    'refgraph': refgraph})
@@ -165,4 +208,3 @@ if __name__ == '__main__':
         createtestsuite(testsuite)
     else:
         evaltestsuite(testsuite)
-    
