130f15dea6c46eb9bf012d19b1a24e7fe8ebd6c9
Staffan Malmgren <staffan.malmgren@gmail.com>
2016-06-28 21:26:11 +0200

    useful script

---
+++ tools/refresh.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+echo "removing old generated files"
+rm -r data/*/parsed
+rm -r data/*/distilled
+rm -r data/*/annotations
+rm -r data/*/generated
+rm -r data/*/toc
+echo "resetting fulltextindex"
+set -e  # fail immediately on error
+./ferenda-build.py devel destroyindex
+echo "resetting triplestore"
+./ferenda-build.py devel clearstore
+echo "updating git sources"
+git pull -q
+echo "building everything"
+./ferenda-build.py all all --processes=7
+echo "deploying to remote"
+cd ..
+fab -H colo.tomtebo.org -f tools/fabfile.py deploy
+
