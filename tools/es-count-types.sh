cd33bca5f3411e27864ef1cfffb840840a74925c
Staffan Malmgren <staffan.malmgren@gmail.com>
2016-08-28 19:19:28 +0200

    display count for all types, not just top 10

--- tools/es-count-types.sh
+++ tools/es-count-types.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl "localhost:9200/lagen/_search?search_type=count&pretty=true" -d '{"aggs": {"count_by_type": {"terms": {"field": "_type"}}}}'
+curl "localhost:9200/lagen/_search?search_type=count&pretty=true" -d '{"aggs": {"count_by_type": {"terms": {"field": "_type", "size": 0, "order": {"_term": "asc"}}}}}'
