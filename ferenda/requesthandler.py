d7194dad9373cd02865f0ef599b31e69d43bb517
Staffan Malmgren <staffan.malmgren@gmail.com>
2016-09-28 13:12:22 +0200

    auto-crop PNG images rendered from PDFs

--- ferenda/requesthandler.py
+++ ferenda/requesthandler.py
@@ -233,11 +233,16 @@ class RequestHandler(object):
                     outfile = repo.store.intermediate_path(basefile, attachment=baseattach)
                     if not os.path.exists(outfile):
                         # params['page'] is 0-based, pdftoppm is 1-based
-                        cmdline = "pdftoppm -f %s -singlefile -png %s %s" % (int(params["page"])+1, sourcefile, outfile.replace(".png",""))
+                        cmdline = "pdftoppm -f %s -singlefile -png %s %s" % (int(params["page"])+1, sourcefile, outfile.replace(".png",".tmp"))
                         util.runcmd(cmdline, require_success=True)
+                        cmdline = "convert %s -trim %s" % (outfile.replace(".png", ".tmp.png"), outfile)
+                        print("running %s" % cmdline)
+                        util.runcmd(cmdline, require_success=True)
+                        os.unlink(outfile.replace(".png", ".tmp.png"))
                 except Exception as e:
                     if not baseattach:
                         baseattach = "page_error.png"
+                    outfile = repo.store.intermediate_path(basefile, attachment=baseattach)
                     errormsg = str(e).replace("\n", "\\n")
                     cmdline = "convert  label:'%s' %s" % (errormsg, outfile)
                     util.runcmd(cmdline, require_success=True)
