ca91f819357c90b0108944ddced3350b50321ef1
Staffan Malmgren <staffan.malmgren@gmail.com>
2016-11-05 21:21:06 +0100

    remove empty sparql warning

--- ferenda/sources/legal/se/direktiv.py
+++ ferenda/sources/legal/se/direktiv.py
@@ -319,6 +319,7 @@ class Direktiv(CompositeRepository, SwedishLegalSource):
     rdf_type = RPUBL.Kommittedirektiv
     documentstore_class = DirektivStore
     sparql_annotations = "sparql/describe-with-subdocs.rq"
+    sparql_expect_results = False
 
     # NB: The same logic as in
     # ferenda.sources.legal.se.{Regeringen,Riksdagen}.metadata_from_basefile
