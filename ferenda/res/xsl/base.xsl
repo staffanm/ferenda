staffanm/ferenda

commit 99cfae94e0ee0882be40fb0c086e6f5d676b8bbf

fixes staffanm/lagen.nu#48

ferenda/res/xsl/base.xsl
@@ -26,15 +26,6 @@
 	      encoding='utf-8'
 	      indent="yes"/>
 
-  <xsl:template match="/">
-    <!-- this is a ugly workaround required to get the proper html5
-         doctype *and* the pre-rootnode conditional IE comments needed
-         for the h5bp template. Strip the <remove-this-tag> start and end
-         tags as a postprocessing step. -->
-    <remove-this-tag>
-      <xsl:apply-templates/>
-    </remove-this-tag>
-  </xsl:template>
   
   <xsl:template match="xhtml:html">
     <html>

ferenda/documentrepository.py
@@ -603,6 +603,7 @@ with the *config* object as single parameter.
         #
         # It might also be impossible to provide the canonical_uri
         # without actually parse()ing the document
+
         return "%sres/%s/%s" % (self.config.url, self.alias, basefile)
 
     def dataset_uri(self, param=None, value=None):

ferenda/sources/general/keyword.py
@@ -8,6 +8,7 @@ import re
 import os
 from collections import defaultdict
 import unicodedata
+import gzip
 
 # 3rdparty libs
 import requests
@@ -65,9 +66,18 @@ class Keyword(DocumentRepository):
 
     def __init__(self, config=None, **kwargs):
         super(Keyword, self).__init__(config, **kwargs)
+
+        self.mediawikirepo = None
+        if self.config._parent and hasattr(self.config._parent, 'mediawiki'):
+            from ferenda.manager import _load_class
+            classname = getattr(self.config._parent.mediawiki, 'class')
+            cls = _load_class(classname)
+            self.mediawikirepo = cls(self.config._parent.mediawiki, keywordrepo=self)
+
         # extra functions -- subclasses can add / remove from this
         self.termset_funcs = [self.download_termset_mediawiki,
                               self.download_termset_wikipedia]
+
 
     @classmethod
     def get_default_options(cls):
@@ -150,43 +160,53 @@ class Keyword(DocumentRepository):
         return term
                 
     def download_termset_mediawiki(self, terms):
-        # 2) Download the wiki.lagen.nu dump from
-        # http://wiki.lagen.nu/pages-articles.xml -- term set "mediawiki"
-        xml = etree.parse(requests.get(self.config.mediawikidump).text)
-        wikinamespaces = []
+        if 'mediawikidump' in self.config:
+            # 2) Download the wiki.lagen.nu dump from
+            # http://wiki.lagen.nu/pages-articles.xml -- term set "mediawiki"
+            xml = etree.parse(requests.get(self.config.mediawikidump).text)
+            wikinamespaces = []
 
-        # FIXME: Handle any MW_NS namespace (c.f. wiki.py)
-
-        for ns_el in xml.findall("//" + MW_NS + "namespace"):
-            wikinamespaces.append(ns_el.text)
-        for page_el in xml.findall(MW_NS + "page"):
-            title = page_el.find(MW_NS + "title").text
-            if title == "Huvudsida":
-                continue
-            if ":" in title and title.split(":")[0] in wikinamespaces:
-                continue  # only process pages in the main namespace
-            if title.startswith("SFS/"):  # FIXME: should be handled in
-                                         # subclass -- or
-                                         # repo-specific pages should
-                                         # be kept in subclasses
-                continue  # only proces normal keywords
-            terms[title]['mediawiki'] = True
+            # FIXME: Handle any MW_NS namespace (c.f. wiki.py)
+            for ns_el in xml.findall("//" + MW_NS + "namespace"):
+                wikinamespaces.append(ns_el.text)
+            for page_el in xml.findall(MW_NS + "page"):
+                title = page_el.find(MW_NS + "title").text
+                if title == "Huvudsida":
+                    continue
+                if ":" in title and title.split(":")[0] in wikinamespaces:
+                    continue  # only process pages in the main namespace
+                terms[title]['mediawiki'] = True
+        elif self.mediawikirepo:
+            for term in self.mediawikirepo.store.list_basefiles_for("parse"):
+                terms[term]['mediawiki'] = True
+        else:
+            self.log.error("Neither mediawikidump or mediawikirepo is defined, "
+                           "can't download mediawiki terms")
 
         self.log.debug("Retrieved subject terms from wiki, now have %s terms" %
                        len(terms))
+
+    def _download_termset_mediawiki_titles(self):
+        # subclasses might want to override this to filter stuff out
+        # from the set of titles
+        for title in self.mediawikirepo.store.list_basefiles_for("parse"):
+            yield title
 
     def download_termset_wikipedia(self, terms):
         # 3) Download the Wikipedia dump from
         # http://download.wikimedia.org/svwiki/latest/svwiki-latest-all-titles-in-ns0.gz
         # -- term set "wikipedia"
-        # FIXME: only download when needed
-        resp = requests.get(self.config.wikipediatitles)
-        wikipediaterms = resp.text.split("\n")
-        for utf8_term in wikipediaterms:
-            term = utf8_term.decode('utf-8').strip()
-            if term in terms:
-                terms[term]['wikipedia'] = True
-
+        filename = self.store.datadir + "/downloaded/wikititles.gz"
+        updated = self.download_if_needed(self.config.wikipediatitles, None, filename=filename)
+        with gzip.open(filename, mode='rt', encoding="utf-8") as fp:
+            # to avoid creating a term for every page on wikipedia,
+            # only register those terms that have already been
+            # featured in another termset. This means that this
+            # function must run last of all termset funcs
+            for term in fp:
+                term = term.strip()
+                if term in terms:
+                    terms[term]['wikipedia'] = True
         self.log.debug("Retrieved terms from wikipedia, now have %s terms" % len(terms))
 
     @managedparsing
@@ -200,8 +220,8 @@ class Keyword(DocumentRepository):
         self.parse_entry_update(doc)
         return True
 
-    re_tagstrip = re.compile(r'<[^>]*>')
 
+    re_tagstrip = re.compile(r'<[^>]*>')
     # FIXME: This is copied verbatim from sfs.py -- maybe it could go
     # into DocumentRepository or util? (or possibly triplestore?)
     def store_select(self, store, query_template, uri, context=None):

ferenda/sources/legal/se/res/xsl/dv.xsl
@@ -240,13 +240,8 @@
        it's in (usually the XHTML1.1 NS) into the default namespace
        -->
   <xsl:template match="*">
-    <xsl:message>Adding element <xsl:value-of select="local-name()"/></xsl:message>
-    <!-- remove element prefix -->
     <xsl:element name="{local-name()}">
-      <!-- process attributes -->
       <xsl:for-each select="@*">
-	<xsl:message>....Adding attribute <xsl:value-of select="local-name()"/></xsl:message>
-        <!-- remove attribute prefix -->
         <xsl:attribute name="{local-name()}">
           <xsl:value-of select="."/>
         </xsl:attribute>

ferenda/sources/legal/se/sfs.py
@@ -1072,7 +1072,7 @@ class SFS(Trips):
     # it's canonical_uri()
     def _term_to_subject(self, term):
         capitalized = term[0].upper() + term[1:]
-        return 'https://lagen.nu/concept/%s' % capitalized.replace(' ', '_')
+        return 'https://lagen.nu/begrepp/%s' % capitalized.replace(' ', '_')
 
     # this struct is intended to be overridable
     ordinalpredicates = {

lagen/nu/keyword.py
@@ -11,6 +11,7 @@ from wsgiref.util import request_uri
 from lxml import etree
 from rdflib.namespace import DCTERMS
 
+from ferenda import util
 from ferenda import TripleStore, Facet, RequestHandler
 from ferenda.sources.general import keyword
 from ferenda.sources.legal.se import SwedishLegalSource, SFS
@@ -19,7 +20,7 @@ class LNKeywordHandler(RequestHandler):
     def supports(self, environ):
         if environ['PATH_INFO'].startswith("/dataset/"):
             return super(LNKeywordHandler, self).supports(environ)
-        return environ['PATH_INFO'].startswith("/concept/")
+        return environ['PATH_INFO'].startswith("/begrepp/")
 
 
 class LNKeyword(keyword.Keyword):
@@ -37,7 +38,7 @@ class LNKeyword(keyword.Keyword):
         super(LNKeyword, self).__init__(config, **kwargs)
         # FIXME: Don't bother with the large wp download right now
         # (but reinstate later)
-        self.termset_funcs.remove(self.download_termset_wikipedia)
+        # self.termset_funcs.remove(self.download_termset_wikipedia)
         if self.config._parent and hasattr(self.config._parent, "sfs"):
             self.sfsrepo = SFS(self.config._parent.sfs)
         else:
@@ -53,10 +54,10 @@ class LNKeyword(keyword.Keyword):
     def canonical_uri(self, basefile):
         # FIXME: make configurable like SFS.canonical_uri
         capitalized = basefile[0].upper() + basefile[1:]
-        return 'https://lagen.nu/concept/%s' % capitalized.replace(' ', '_')
+        return 'https://lagen.nu/begrepp/%s' % capitalized.replace(' ', '_')
 
     def basefile_from_uri(self, uri):
-        prefix = "https://lagen.nu/concept/"
+        prefix = "https://lagen.nu/begrepp/"
         if prefix in uri:
             return uri.replace(prefix, "").replace("_", " ")
         else:
@@ -108,12 +109,21 @@ class LNKeyword(keyword.Keyword):
         for l in legaldefs:
             subject_node = etree.SubElement(main_node,
                                             ns("rinfoex:isDefinedBy"))
-            rattsfall_node = etree.SubElement(subject_node,
+            legaldef_node = etree.SubElement(subject_node,
                                               ns("rdf:Description"))
-            rattsfall_node.set(ns("rdf:about"), l['uri'])
-            id_node = etree.SubElement(rattsfall_node, ns("rdfs:label"))
+            legaldef_node.set(ns("rdf:about"), l['uri'])
+            id_node = etree.SubElement(legaldef_node, ns("rdfs:label"))
             # id_node.text = "%s %s" % (l['uri'].split("#")[1], l['label'])
             id_node.text = self.sfsrepo.display_title(l['uri'])
+
+        if 'wikipedia\n' in util.readfile(self.store.downloaded_path(basefile)):
+            subject_node = etree.SubElement(main_node,
+                                            ns("rdfs:seeAlso"))
+            link_node = etree.SubElement(subject_node,
+                                         ns("rdf:Description"))
+            link_node.set(ns("rdf:about"), 'http://sv.wikipedia.org/wiki/' + basefile.replace(" ","_"))
+            label_node = etree.SubElement(link_node, ns("rdfs:label"))
+            label_node.text = "Begreppet %s finns även beskrivet på svenska Wikipedia" % basefile
 
     def facets(self):
         def kwselector(row, binding, resource_graph):

lagen/nu/res/xsl/annotations-panel.xsl
@@ -8,7 +8,6 @@
     <xsl:param name="panelid"/>
     <xsl:param name="expanded" select="false()"/>
     <xsl:variable name="expanded-class"><xsl:if test="$expanded">in</xsl:if></xsl:variable>
-    <xsl:message><xsl:value-of select="$paneltype"/>-<xsl:value-of select="$panelid"/>: expanded-class='<xsl:value-of select="$expanded-class"/>'</xsl:message>
     <div class="panel panel-default">
       <div class="panel-heading" role="tab" id="heading-{$paneltype}-{$panelid}">
 	<h4 class="panel-title">
@@ -27,8 +26,8 @@
 	       transform the namespace from
 	       http://www.w3.org/1999/xhtml to the default namespace
 	       (and avoid declaring a whole mess of namespaces used in
-	       the source doc -->
-	  <xsl:apply-templates select="$nodeset"/>
+	       the source doc) -->
+	    <xsl:apply-templates select="$nodeset"/>
 	  <!-- <xsl:copy-of select="$nodeset"/> -->
 	</xsl:if>
       </div>

lagen/nu/res/xsl/keyword.xsl
@@ -8,7 +8,6 @@
 		xmlns:rinfo="http://rinfo.lagrummet.se/taxo/2007/09/rinfo/pub#"
 		xmlns:rinfoex="http://lagen.nu/terms#"
 		xmlns:ext="http://exslt.org/common"
-		xml:space="preserve"
 		exclude-result-prefixes="xhtml rdf">
 
   <xsl:import href="uri.xsl"/>
@@ -29,7 +28,10 @@
     this point to add all of our content -->
     <div class="section-wrapper toplevel">
       <section class="col-sm-7">
-	<xsl:apply-templates select="$myannotations/rdf:Description/dcterms:description/xhtml:div"/>
+	<xsl:choose><xsl:when test="count($myannotations/rdf:Description/dcterms:description/xhtml:div) > 0">
+	  <xsl:apply-templates select="$myannotations/rdf:Description/dcterms:description/xhtml:div"/>
+	</xsl:when><xsl:otherwise><p class="alert alert-warning">Beskrivning saknas!</p></xsl:otherwise>
+	</xsl:choose>
       </section>
       <xsl:call-template name="aside-annotations">
 	<xsl:with-param name="uri" select="@about"/>
@@ -41,18 +43,16 @@
   <xsl:param name="content-under-pagetitle" select="true()"/>
       
 
-  <xsl:template match="xhtml:a">
-    <xsl:call-template name="link"/>
-  </xsl:template>
-
   <xsl:template name="aside-annotations">
     <xsl:param name="uri"/>
+    <xsl:variable name="links" select="$myannotations/rdf:Description/rdfs:seeAlso/*"/>
+    <xsl:message>Found <xsl:value-of select="count($links)"/> link</xsl:message>
     <xsl:variable name="legaldefinitioner" select="$myannotations/rdf:Description/rinfoex:isDefinedBy/*"/>
     <xsl:variable name="rattsfall" select="$myannotations/rdf:Description/dcterms:subject/rdf:Description"/>
     <xsl:variable name="rattsfall-markup">
       <ul>
 	<xsl:for-each select="$rattsfall">
-	  <li><a href="{@rdf:about}"><b><xsl:value-of select="dcterms:identifier"/></b></a>:
+	  <li><xhtml:a href="{@rdf:about}"><b><xsl:value-of select="dcterms:identifier"/></b></xhtml:a>:
 	  <xsl:value-of select="dcterms:description"/></li>
 	</xsl:for-each>
       </ul>
@@ -60,13 +60,32 @@
     <xsl:variable name="legaldefinitioner-markup">
       <ul>
 	<xsl:for-each select="$legaldefinitioner">
-	  <li><a href="{@rdf:about}"><xsl:value-of select="rdfs:label"/></a></li>
+	  <li><xhtml:a href="{@rdf:about}"><xsl:value-of select="rdfs:label"/></xhtml:a></li>
+	</xsl:for-each>
+      </ul>
+    </xsl:variable>
+    <xsl:variable name="links-markup">
+      <ul>
+	<xsl:for-each select="$links">
+	  <xsl:variable name="href" select="@rdf:about"/>
+	  <li><xhtml:a href="{@rdf:about}"><xsl:value-of select="rdfs:label"/></xhtml:a></li>
 	</xsl:for-each>
       </ul>
     </xsl:variable>
 
     <aside class="panel-group col-sm-5" role="tablist" id="panel-top"
 	   aria-multiselectable="true">
+      <xsl:if test="$links">
+	<xsl:call-template name="aside-annotations-panel">
+	  <xsl:with-param name="title">Länkar</xsl:with-param>
+	  <!-- <xsl:with-param name="badgecount" select="count($rattsfall)"/> -->
+	  <xsl:with-param name="nodeset" select="ext:node-set($links-markup)"/>
+	  <xsl:with-param name="panelid">top</xsl:with-param>
+	  <xsl:with-param name="paneltype">links</xsl:with-param>
+	  <xsl:with-param name="expanded" select="true()"/>
+	</xsl:call-template>
+      </xsl:if>
+
       <xsl:if test="$rattsfall">
 	<xsl:call-template name="aside-annotations-panel">
 	  <xsl:with-param name="title">Rättsfall</xsl:with-param>
@@ -97,13 +116,18 @@
 
   <!-- remove spans which only purpose is to contain RDFa data -->
   <xsl:template match="xhtml:span[@property and @content and not(text())]"/>
-  
   <!-- default template: translate everything from whatever namespace
        it's in (usually the XHTML1.1 NS) into the default namespace
-       NOTE: It removes any attributes not accounted for otherwise
-       -->
+  -->
   <xsl:template match="*">
-    <xsl:element name="{local-name(.)}"><xsl:apply-templates select="node()"/></xsl:element>
+    <xsl:element name="{local-name()}">
+      <xsl:for-each select="@*">
+        <xsl:attribute name="{local-name()}">
+          <xsl:value-of select="."/>
+        </xsl:attribute>
+      </xsl:for-each>
+      <xsl:apply-templates/>
+    </xsl:element>
   </xsl:template>
 
   <!-- toc handling (do nothing) -->

commit 47532ab3f64c1ac4c85bcf311861bc7fd1a122fe

upgrade to Bootstrap 3.3.7, and fix scrollspy-related navbar regression introduced in 99cfae9

ferenda/res/xsl/base.xsl
@@ -26,7 +26,16 @@
 	      encoding='utf-8'
 	      indent="yes"/>
 
-  
+  <xsl:template match="/">
+    <!-- this is a ugly workaround required to get the proper html5
+         doctype *and* the pre-rootnode conditional IE comments needed
+         for the h5bp template. Strip the <remove-this-tag> start and end
+         tags as a postprocessing step. -->
+    <remove-this-tag>
+      <xsl:apply-templates/>
+    </remove-this-tag>
+  </xsl:template>
+      
   <xsl:template match="xhtml:html">
     <html>
     <xsl:apply-templates/>
@@ -39,9 +48,7 @@
       <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <title><xsl:call-template name="headtitle"/></title>
-
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous"/>
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous"/>
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"/>
       <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
       <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
       <!--[if lt IE 9]>
@@ -144,9 +151,8 @@
 	  </nav>
 	</footer>
       </div>
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js">&#160;</script>
-      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous">&#160;</script>
-      <!-- <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js">&#160;</script> -->
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js">&#160;</script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous">&#160;</script>
       <script src="http://hammerjs.github.io/dist/hammer.min.js">&#160;</script>
       <xsl:if test="$dyntoc">
 	<script src="https://cdn.rawgit.com/afeld/bootstrap-toc/v0.3.0/dist/bootstrap-toc.min.js">&#160;</script> 

ferenda/sources/legal/se/res/xsl/forarbete.xsl
@@ -13,7 +13,7 @@ really tested with direktiv, utredningar (SOU/Ds) and propositioner.
 		xmlns:bibo="http://purl.org/ontology/bibo/"
 		xmlns:rinfo="http://rinfo.lagrummet.se/taxo/2007/09/rinfo/pub#"
 		xmlns:rinfoex="http://lagen.nu/terms#"
-		exclude-result-prefixes="xhtml rdf rdfs prov">
+		exclude-result-prefixes="xhtml rdf rdfs prov bibo">
 
   <xsl:import href="uri.xsl"/>
   <xsl:include href="base.xsl"/>
@@ -133,7 +133,7 @@ really tested with direktiv, utredningar (SOU/Ds) and propositioner.
          things, ie the Protokollsutdrag structure of older
          propositions (c.f. prop 1990/91:172) -->
     <div class="row toplevel">
-      <section id="{translate(substring-after(@about,'#'),'.','-')}" class="col-sm-8">
+      <section id="{substring-after(@about,'#')}" class="col-sm-8">
 	<xsl:if test="@content">
 	  <h2><xsl:value-of select="@content"/></h2>
 	</xsl:if>
@@ -156,7 +156,7 @@ really tested with direktiv, utredningar (SOU/Ds) and propositioner.
        (with a URI) gets a <section> with an <aside> for inbound links etc -->
   <xsl:template match="xhtml:div[@about and (@class='section' or @class='preamblesection' or @class='unorderedsection')]">
     <div class="row" about="{@about}"><!-- needed? -->
-      <section id="{translate(substring-after(@about,'#'),'.','-')}" class="col-sm-8">
+      <section id="{substring-after(@about,'#')}" class="col-sm-8">
 	<xsl:variable name="sectionheading"><xsl:if test="xhtml:span/@content"><xsl:value-of select="xhtml:span/@content"/>. </xsl:if><xsl:value-of select="@content"/></xsl:variable>
 	<xsl:if test="count(ancestor::*) = 2">
 	    <h2><xsl:value-of select="$sectionheading"/></h2>
@@ -204,7 +204,7 @@ really tested with direktiv, utredningar (SOU/Ds) and propositioner.
     <xsl:if test="string-length(@content) > 0">
       <h3><xsl:value-of select="@content"/></h3>
     </xsl:if>
-    <div class="forfattningskommentar" id="{translate(substring-after(@about, '#'),'.','-')}">
+    <div class="forfattningskommentar" id="{substring-after(@about, '#')}">
       <xsl:for-each select="xhtml:div/xhtml:div/*">
 	<xsl:apply-templates/>
 	<xsl:if test="@class='sidbrytning'">
@@ -287,7 +287,7 @@ really tested with direktiv, utredningar (SOU/Ds) and propositioner.
       </xsl:if>
       <xsl:value-of select="@content"/>
     </xsl:variable>
-    <li><a href="#{translate(substring-after(@about, '#'), '.', '-')}"><xsl:value-of select="$label"/></a>
+    <li><a href="#{substring-after(@about, '#')}"><xsl:value-of select="$label"/></a>
     <xsl:if test="xhtml:div[@typeof='bibo:DocumentPart']">
       <ul class="nav">
 	<xsl:apply-templates mode="toc"/>