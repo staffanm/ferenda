fe266a65f42501e931fc48f2e10af017893a110a
Staffan Malmgren <staffan.malmgren@gmail.com>
2017-09-26 21:25:32 +0200

    appveyor debugging

--- tools/rununittest.py
+++ tools/rununittest.py
@@ -2,6 +2,9 @@
 # "python -m unittest", but avoids http://bugs.python.org/issue10845
 # which hands the multiprocessing tests on windows/py2
 from unittest.main import main
-
+import sys
+from pprint import pprint
 if __name__ == '__main__':
+    sys.path.append(".")
+    pprint(sys.path)
     main(module=None)
