7586b28c15b2a2ed21512861042db98c26365d9d
Staffan Malmgren <staffan.malmgren@gmail.com>
2018-06-07 21:33:41 +0200

    updated metadataonly options

--- tools/update-options.py
+++ tools/update-options.py
@@ -14,17 +14,21 @@ optionsfile = sys.argv[1]
 assert os.path.exists(optionsfile)
 repos = sys.argv[2:]
 assert repos
-remove = False  # set this to True manually when you're really really sure
+remove = True  # set this to True manually when you're really really sure
+count= {"unreferenced": 0,
+        "metadataonly": 0,
+        "removed": 0}
 
 # 1 load ferenda.conf to instantiate repos with proper options
 configpath = "ferenda.ini"
 config = _load_config(configpath)
-
+insts = {}
 # 2 for each repo, list all available basefiles
 metadataonly = []
 for repo in repos:
     cls = _load_class(_enabled_classes("ferenda.ini")[repo])
     inst = _instantiate_class(cls, config)
+    insts[repo] = inst
     for basefile in inst.store.list_basefiles_for("parse"):
         #   2.1 if deppath(basefile) doesn't exist or is empty
         deppath = inst.store.dependencies_path(basefile)
@@ -32,8 +36,9 @@ for repo in repos:
             print("%s %s: unreferenced document" % (repo, basefile))
             #   2.3 add (repo, basefile) to internal metadataonly-list
             metadataonly.append((repo, basefile))
+count["unreferenced"] = len(metadataonly)            
 
-
+removed = []
 if metadataonly:
     # 3 load (eval) options file, then reopen("aw") and seek to end - 1 (don't read last '}')
     options = ast.literal_eval(util.readfile(optionsfile))
@@ -55,26 +60,36 @@ if metadataonly:
             if (repo, basefile) not in options and filter(repo, basefile):
                 print("%s %s: Setting to metadataonly" % (repo, basefile))
                 fp.write('    ("%s", "%s"): "metadataonly",\n' % (repo, basefile))
+                count["metadataonly"] += 1
                 if remove:
-                    downloaded_path = inst.store.downloaded_path(basefile)
-                    storage_policy = inst.store.storage_policy
-                    if not os.path.exists(downloaded_path):
-                        # maybe the reason is that this is a compositerepo?
-                        # FIXME: maybe CompositeStore.downloaded_path and
-                        # friends should do this transparently?
-                        if hasattr(inst, 'get_preferred_instances'):
-                            subinst = list(inst.get_preferred_instances(basefile))[0]
-                            downloaded_path = subinst.store.downloaded_path(basefile)
-                            storage_policy = subinst.store.storage_policy
-                    assert(os.path.exists(downloaded_path))
-                    print("%s %s: removing %s" % (repo, basefile, downloaded_path))
-                    if storage_policy == "dir":
-                        shutil.rmtree(os.path.dirname(downloaded_path))
-                    else:
-                        util.robust_remove(downloaded_path)
-
+                    inst = insts[repo]
+                    removed.append((inst, basefile))
         fp.write('}\n')
 
+for (inst, basefile) in removed:
+    downloaded_path = inst.store.downloaded_path(basefile)
+    storage_policy = inst.store.storage_policy
+    if not os.path.exists(downloaded_path):
+        # maybe the reason is that this is a compositerepo?
+        # FIXME: maybe CompositeStore.downloaded_path and
+        # friends should do this transparently?
+        if hasattr(inst, 'get_preferred_instances'):
+            subinsts = list(inst.get_preferred_instances(basefile))
+            if not subinsts:
+                print("%s %s: WARNING: no subinst handles this basefile" % (inst.alias, basefile))
+                continue
+            subinst = subinsts[0]
+            downloaded_path = subinst.store.downloaded_path(basefile)
+            storage_policy = subinst.store.storage_policy
+    assert(os.path.exists(downloaded_path))
+    print("%s %s: removing %s" % (inst.alias, basefile, downloaded_path))
+    count["removed"] += 1
+    if storage_policy == "dir":
+        shutil.rmtree(os.path.dirname(downloaded_path))
+        # print("shutil.rmtree(%s)" % os.path.dirname(downloaded_path))
+    else:
+        util.robust_remove(downloaded_path)
+        # print("util.robust_remove(%s)" % downloaded_path)
 
-
+print("%(unreferenced)s unreferenced docs, %(metadataonly)s set to 'metadataonly', %(removed)s removed" % count)
 
