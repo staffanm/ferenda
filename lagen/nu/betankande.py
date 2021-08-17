6f5690b939aa251d2e47dc71cd12a1d0618095a7
Egil <egil.moller@freecode.no>
2021-08-17 21:59:52 +0200

    Bugfix

--- lagen/nu/betankande.py
+++ lagen/nu/betankande.py
@@ -125,21 +125,21 @@ class Betankande(DocumentRepository):
         desc.value(self.ns['dcterms'].identifier, "Betänkande " + bet["id"])
 
         for url in bet["Riksdagsskrivelse"]["links"].values():
-            desc.value(self.ns["parliament"].communication, URIRef(url))
+            desc.rel(self.ns["parliament"].communication, URIRef(url))
         for url in bet["Protokoll med beslut"]["links"].values():
-            desc.value(self.ns["parliament"].record, URIRef(url))
+            desc.rel(self.ns["parliament"].record, URIRef(url))
 
         for title, proposal in bet["Förslagspunkter och beslut i kammaren"].items():
             with desc.rel(self.ns["dcterms"].hasPart, BNode(urllib.parse.quote(title))):
                 desc.value(self.ns['dcterms'].title, title)
                 for key, value in proposal.items():
                     if key in ("votering", "resultat"): continue
-                    desc.value({True: self.ns["parliament"].approve,
+                    desc.rel({True: self.ns["parliament"].approve,
                                 False: self.ns["parliament"].reject,
                                 "bifaller-delvis": self.ns["parliament"].partial}[value],
                                URIRef(key))
                 if proposal["votering"] == "acklamation":
-                    desc.value(self.ns["dcterms"].creator, self.ns["parliament"].acclamation)
+                    desc.rel(self.ns["dcterms"].creator, URIRef(self.ns["parliament"].acclamation))
                 else:
                     with desc.rel(self.ns["dcterms"].creator, proposal["votering"]):
                         with open(os.path.join(dirname, proposal["votering"].split("/")[-1])) as f:
@@ -151,18 +151,18 @@ class Betankande(DocumentRepository):
 
                         for idx, row in votes[["Parti", "Röst"]].value_counts().rename("count").reset_index().iterrows():
                             with desc.rel(self.ns["dcterms"].hasPart, BNode(urllib.parse.quote("%(Parti)s-%(Röst)s" % row))):
-                                desc.value(self.ns["parliament"].party, URIRef("http://rinfo.lagrummet.se/org/riksdag/member/%(Parti)s" % row))
-                                desc.value(self.ns["parliament"].vote,
-                                           {"Ja": self.ns["parliament"].yes,
-                                            "Nej": self.ns["parliament"].no,
-                                            "Frånvarande": self.ns["parliament"].absent
+                                desc.rel(self.ns["parliament"].party, URIRef("http://rinfo.lagrummet.se/org/riksdag/member/%(Parti)s" % row))
+                                desc.rel(self.ns["parliament"].vote,
+                                           {"Ja": URIRef(self.ns["parliament"].yes),
+                                            "Nej": URIRef(self.ns["parliament"].no),
+                                            "Frånvarande": URIRef(self.ns["parliament"].absent)
                                            }[row["Röst"]])
                                 desc.value(self.ns["parliament"]["count"], row["count"])
                     
-                desc.value(self.ns["dcterms"].valid,
-                           {True: self.ns["parliament"].yes,
-                            False: self.ns["parliament"].no,
-                            "partial": self.ns["parliament"].partial
+                desc.rel(self.ns["dcterms"].valid,
+                           {True: URIRef(self.ns["parliament"].yes),
+                            False: URIRef(self.ns["parliament"].no),
+                            "partial": URIRef(self.ns["parliament"].partial)
                            }[proposal["resultat"]])
 
         #print(doc.meta.serialize(format="trig").decode("utf-8"))
